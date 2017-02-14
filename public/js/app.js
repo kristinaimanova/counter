6d37678b829024666c6d5f104e646b26515bc43d
Kristina Ivanova <kristinaivanova@Kristinas-Air.itpro.tv>
2017-02-13 20:57:17 -0500

    added more styling in main.css and added a reset button and gave it functionality

--- public/js/app.js
+++ public/js/app.js
@@ -10,10 +10,10 @@ cacheDom: function(){
     counter.subtractButton = document.getElementById('subtractButton');
     counter.addButton = document.getElementById('addButton');
     counter.countDisplay = document.getElementById('counter');
+    counter.reset = document.getElementById('reset');
     counter.count = 0;
 },
 render: function(){
-
     counter.countDisplay.innerHTML = counter.count;
 },
 addEventListeners: function(){
@@ -23,6 +23,10 @@ addEventListeners: function(){
     });
     counter.subtractButton.addEventListener('click', function(){
         counter.count -= 1;
+        counter.render();
+    });
+    counter.reset.addEventListener('click', function(){
+        counter.count = 0;
         counter.render();
     });
 }
